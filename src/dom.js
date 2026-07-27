fa16a35b7a16d6ba8e68f66a76b2d1fd5431d756
Aurel-Charles <charlesaurelienpro@gmail.com>
2026-07-27 19:02:59 +0200

    fix gameOver and add clearWinner()

--- src/dom.js
+++ src/dom.js
@@ -52,3 +52,4 @@ export function updateBoard(player, showShips = false) {
         
     }   
 }
+
--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import "./style.css"
 console.log("Hello Odin!");
 
 
-
-const game = new Game('aurel', 'computer', [5,4,3,3,2])
-let gameOver = false
-
 function attackCell(x,y, isComputer = false) {
     if (gameOver) {
         return
@@ -36,8 +32,9 @@ function attackCell(x,y, isComputer = false) {
     }
 
     if (game.playerTwo.board.allShipSunk) {
-        alert('player One WIN')
         gameOver = true
+        winner = game.playerOne.name
+        renderWinner()
         return
     }
     
@@ -46,18 +43,35 @@ function attackCell(x,y, isComputer = false) {
     updateBoard(game.playerOne, true)
     
     if (game.playerOne.board.allShipSunk) {
-        alert('Computer WIN')
         gameOver = true
+        winner = game.playerTwo.name
+        renderWinner()
         return
     }
 }
 
+function resetGame(game) {
+    gameOver = false
+    game.playerOne.board.reset()
+    game.playerTwo.board.reset()
+    game.playerOne.randomPlaceShip(game.shipsByLength)
+    game.playerTwo.randomPlaceShip(game.shipsByLength)
+    clearWinner(true)
+}
+
+
+
+
+// init the game
+const game = new Game('aurel', 'computer', [5,4,3,3,2])
+let gameOver = false
+let winner
 game.playerOne.randomPlaceShip(game.shipsByLength)
 game.playerTwo.randomPlaceShip(game.shipsByLength)
 
-
-
 const mainDiv = document.querySelector('.main') 
+
+// render boards on page
 const boardPlayerOne = renderBoard(game.playerOne.board, game.playerOne.name)
 const boardPlayerTwo = renderBoard(game.playerTwo.board, game.playerTwo.name, attackCell)
 
@@ -66,3 +80,29 @@ mainDiv.append(boardPlayerOne, boardPlayerTwo)
 updateBoard(game.playerOne, true)
 updateBoard(game.playerTwo)
 
+// render button reset game
+const btnResetGame = document.createElement('button')
+btnResetGame.classList.add('btn-reset')
+btnResetGame.textContent = 'Reset Game'
+btnResetGame.addEventListener('click', ()=> {
+    resetGame(game)
+    updateBoard(game.playerOne, true)
+    updateBoard(game.playerTwo)
+})
+mainDiv.append(btnResetGame)
+
+const winnerEl = document.createElement('div')
+winnerEl.classList.add('winner')
+const winnerTextEl = document.createElement('p')
+winnerTextEl.classList.add('winner-text')
+winnerEl.append(winnerTextEl)
+mainDiv.append(winnerEl)
+
+
+// render winner div
+function renderWinner() {
+        winnerTextEl.textContent =  `${winner} win!!`
+}
+function clearWinner() {   
+    winnerTextEl.textContent =  ``
+}
